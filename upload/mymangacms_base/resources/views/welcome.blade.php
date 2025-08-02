734c2b89f4dbce4a079b677b8003471683430bbf
dsgops499 <danielproffe9999@gmail.com>
2025-08-02 22:45:08 +0500

    Add welcome.blade.php file

---
+++ upload/mymangacms_base/resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Laravel 11</title>
+</head>
+<body>
+    <h1>Laravel 11 успешно установлен!</h1>
+    <p>Добро пожаловать в новое приложение.</p>
+</body>
+</html>
